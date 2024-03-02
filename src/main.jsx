7aef7be2026390c845472c9b4eae848d3c3b856e
prabirstack <prabirsacademy@gmail.com>
2024-03-02 11:11:13 +0530

    project UsPopcorn with React - Api data fetching

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+// import StarRating from './components/StarRating'
+
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0)
+//   return (
+//     <div>
+//       <StarRating color='blue' maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   )
+// }
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <StarRating
+      maxRating={5}
+      messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+    />
+    <StarRating size={24} color='red' className='test' defaultRating={3} />
+    <Test /> */}
+    <App />
+  </React.StrictMode>
+)
